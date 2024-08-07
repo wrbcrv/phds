3b9c4199294d31610aaeb8eba4329ebc506d9dbb
Werbton Carvalho <84273781+wrbcrv@users.noreply.github.com>
2024-08-06 22:13:28 -0300

    Atualizando sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,27 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'phds-sidenav',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterModule
-  ],
+  imports: [],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-  drawerOpen: string | null = null;
-
-  toggleDrawer(menuItem: string | null) {
-    if (this.drawerOpen === menuItem) {
-      this.drawerOpen = null;
-    }
-
-    if (this.drawerOpen !== menuItem) {
-      this.drawerOpen = menuItem;
-    }
-  }
+  
 }
