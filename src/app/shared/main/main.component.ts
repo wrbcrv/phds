3e919b26d30a8eed36a27d89a63cf947faafe750
Werbton Carvalho <84273781+wrbcrv@users.noreply.github.com>
2024-09-23 15:43:28 -0300

    Sidenav empurrando conteúdo

--- src/app/shared/main/main.component.ts
+++ src/app/shared/main/main.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { SidenavComponent } from '../sidenav/sidenav.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'phds-main',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     RouterOutlet,
     SidenavComponent
   ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  @ViewChild(SidenavComponent) sidenav!: SidenavComponent;
 
+  isSidenavActive() {
+    return this.sidenav && this.sidenav.active && this.sidenav.active.sub;
+  }
 }
