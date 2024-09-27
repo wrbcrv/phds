a7b6ca7a3430bbff15c7c8a86b74f91b9eb1e61d
Werbton Carvalho <84273781+wrbcrv@users.noreply.github.com>
2024-09-27 16:06:16 -0300

    Alterações na tabela de chamados

--- src/app/components/tickets/ticket-list/ticket-list.component.ts
+++ src/app/components/tickets/ticket-list/ticket-list.component.ts
@@ -11,7 +11,6 @@ import { TicketService } from '../../../services/ticket.service';
 import { PaginationComponent } from '../../../shared/pagination/pagination.component';
 import { SelectComponent } from '../../../shared/select/select.component';
 import { PRIORITY_TRANSLATION_MAP, STATUS_TRANSLATION_MAP } from '../../../shared/translations/translations';
-import { MessagePreviewComponent } from '../message-preview/message-preview.component';
 import { LocationService } from '../../../services/location.service';
 
 @Component({
@@ -21,7 +20,6 @@ import { LocationService } from '../../../services/location.service';
     CommonModule,
     FormsModule,
     SelectComponent,
-    MessagePreviewComponent,
     RouterModule,
     PaginationComponent,
     TippyDirective
